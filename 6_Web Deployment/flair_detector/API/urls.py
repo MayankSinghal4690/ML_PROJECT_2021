14b95aadadd2c059519f72f63a041d31c42d9893
MayankSinghal4690 <mayank.singhal.4690@gmail.com>
2021-09-18 00:40:15 +0530

    Changes_in_files

--- 6_Web Deployment/flair_detector/API/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from API import views
-
-urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^statistics/$', views.statistics, name="statistics")
-    ]
